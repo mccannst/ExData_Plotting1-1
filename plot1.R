35a22d07628a5ca229994189ecd31a06d25ea2dd
Sean McCann <smccann@gmail.com>
2014-06-08 12:06:27 -0700

    second plot commit

--- plot1.R
+++ plot1.R
@@ -1,7 +1,11 @@
 hpcfeb07 <- subset(read.table("household_power_consumption.txt", header=T, sep=";"), 
-                   Date == "2/1/2007" | Date == "2/2/2007", na.strings = ("?"))
+                   Date == "1/2/2007" | Date == "2/2/2007", na.strings = ("?"))
 
 hpcfeb07$Global_active_power <- as.numeric(as.character(hpcfeb07$Global_active_power))
+
+hpcfeb07$DateTime <- paste(hpcfeb07$Date, hpcfeb07$Time)
+
+hpcfeb07$DateTime <- strptime(hpcfeb07$DateTime, "%d/%m/%Y %H:%M:%S")
 
 png(filename = "plot1.png", width=480, height=480)
 
