3aaf00c75c804250831aeaf460e0e1033488d99a
Sean McCann <smccann@gmail.com>
2014-06-08 12:09:48 -0700

    third commit

---
+++ plot2.R
@@ -0,0 +1,16 @@
+hpcfeb07 <- subset(read.table("household_power_consumption.txt", header=T, sep=";"), 
+                   Date == "1/2/2007" | Date == "2/2/2007", na.strings = ("?"))
+
+hpcfeb07$Global_active_power <- as.numeric(as.character(hpcfeb07$Global_active_power))
+
+hpcfeb07$DateTime <- paste(hpcfeb07$Date, hpcfeb07$Time)
+
+hpcfeb07$DateTime <- strptime(hpcfeb07$DateTime, "%d/%m/%Y %H:%M:%S")
+
+
+png(filename = "plot2.png", width=480, height=480)
+
+with(hpcfeb07, plot(DateTime, Global_active_power, type="l", xlab="",
+                    ylab="Global Active Power (kilowatts)"))
+
+dev.off()
